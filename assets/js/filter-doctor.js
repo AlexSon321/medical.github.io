316660363fc6a44bea22dcfd9ede60d1ccbc7712
AlexSon321 <104098000+AlexSon321@users.noreply.github.com>
2022-07-30 09:56:45 +0300

    Update filter-doctor.js

--- assets/js/filter-doctor.js
+++ assets/js/filter-doctor.js
@@ -1,6 +1,7 @@
 const openUp = document.getElementById('silka')
 const closeUp = document.getElementById('close_up')
 const popUp = document.getElementById('pole_voda')
+const all = document.getElementById('all')
 
 openUp.addEventListener('click', function (e) {
     e.preventDefault();
@@ -12,6 +13,7 @@ closeUp.addEventListener('click', () => {
 })
 
 
+
 const list = document.querySelector('.list')
 const items = document.querySelectorAll('.doctor-item')
 
@@ -19,6 +21,17 @@ function filter() {
     list.addEventListener('click', event => {
         const targetId = event.target.dataset.id
         switch (targetId) {
+            case 'all':
+                items.forEach(item => {
+                    if (item.classList.contains('doctor-item')) {
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                })
+
+
+                break
             case 'ryno':
                 items.forEach(item => {
                     if (item.classList.contains('general')) {
